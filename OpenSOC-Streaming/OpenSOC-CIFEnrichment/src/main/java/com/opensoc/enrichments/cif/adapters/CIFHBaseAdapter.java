781c44d9b69fa2dae87720d2d3a986c3d5e24c7c
james-sirota <james.sirota@gmail.com>
2014-06-25 10:57:28 -0500

    Initial Commit of baseline 0.1_beta
    Initial code commit

---
+++ OpenSOC-Streaming/OpenSOC-CIFEnrichment/src/main/java/com/opensoc/enrichments/cif/adapters/CIFHBaseAdapter.java
@@ -0,0 +1,29 @@
+package com.opensoc.enrichments.cif.adapters;
+
+import org.slf4j.Logger;
+
+import com.opensoc.enrichment.interfaces.CIFAdapter;
+
+public class CIFHBaseAdapter implements CIFAdapter{
+
+	public boolean initializeAdapter(String ip, Logger LOG) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String enrichByIP(String metadata) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String enrichByDomain(String metadata) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String enrichByEmail(String metadata) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ OpenSOC-Streaming/OpenSOC-LancopeEnrichment/src/main/java/com/opensoc/enrichments/lancope/adapters/LancopeHbaseAdapter.java
@@ -0,0 +1,19 @@
+package com.opensoc.enrichments.lancope.adapters;
+
+import org.slf4j.Logger;
+
+import com.opensoc.enrichment.interfaces.LancopeAdapter;
+
+public class LancopeHbaseAdapter implements LancopeAdapter{
+
+	public String enrich(String metadata) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean initializeAdapter(String ip, Logger LOG) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}

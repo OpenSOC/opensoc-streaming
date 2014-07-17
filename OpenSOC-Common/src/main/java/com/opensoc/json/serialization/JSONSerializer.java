4b59281e2b8f4eb2ffefce8451eb732a89bc92f1
Kiran K <kiran@tupl.io>
2014-07-16 22:40:31 -0700

    JSON Object Serializer 	new file:   OpenSOC-Common/src/main/java/com/opensoc/json/serialization/JSONSerializer.java 	modified:   OpenSOC-Topologies/src/main/resources/BroExampleOutput

---
+++ OpenSOC-Common/src/main/java/com/opensoc/json/serialization/JSONSerializer.java
@@ -0,0 +1,52 @@
+package com.opensoc.json.serialization;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+import org.json.simple.JSONObject;
+
+import com.esotericsoftware.kryo.serializers.MapSerializer;
+
+/**
+ * @author kiran Custom Serializer to help Storm encode and decode JSONObjects
+ */
+
+public class JSONSerializer extends
+		com.esotericsoftware.kryo.Serializer<JSONObject> {
+
+	@Override
+	public void write(Kryo kryo, Output output, JSONObject json) {
+
+		HashMap<String, String> jsonMap = (HashMap) json;
+
+		// put number of entries
+		output.writeInt(jsonMap.size());
+
+		for (Map.Entry<String, String> entry : jsonMap.entrySet()) {
+			output.writeString(entry.getKey());
+			output.writeString(entry.getValue());
+		}
+	}
+
+	@Override
+	public JSONObject read(Kryo kryo, Input input, Class<JSONObject> type) {
+
+		JSONObject json = new JSONObject();
+
+		// Get number of Entries
+		int size = input.readInt();
+
+		for (int i = 0; i < size; i++) {
+			String key = input.readString();
+			String val = input.readString();
+			json.put(key, val);
+		}
+
+		return json;
+	}
+}

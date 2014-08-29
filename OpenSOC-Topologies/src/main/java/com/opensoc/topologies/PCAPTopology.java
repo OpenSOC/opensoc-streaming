bba5890c00d229e7d31cb934bd525bd469b449ef
james-sirota <james.sirota@gmail.com>
2014-08-29 14:36:37 -0500

    fixed broken build

--- OpenSOC-Topologies/src/main/java/com/opensoc/topologies/PCAPTopology.java
+++ OpenSOC-Topologies/src/main/java/com/opensoc/topologies/PCAPTopology.java
@@ -21,14 +21,24 @@ import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.PropertiesConfiguration;
 import org.json.simple.JSONObject;
 
+import storm.kafka.BrokerHosts;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.ZkHosts;
 import backtype.storm.Config;
+import backtype.storm.spout.RawMultiScheme;
+import backtype.storm.spout.RawScheme;
+import backtype.storm.spout.SchemeAsMultiScheme;
 import backtype.storm.topology.TopologyBuilder;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.opensoc.json.serialization.JSONKryoSerializer;
+import com.opensoc.test.spouts.GenericInternalTestSpout;
 import com.opensoc.topologyhelpers.SettingsLoader;
 
 public class PCAPTopology {
-	
+
 	static Configuration config;
 	static TopologyBuilder builder;
 	static String component = "Spout";
@@ -46,14 +56,20 @@ public class PCAPTopology {
 			config_path = "OpenSOC_Configs";
 		}
 
-		String topology_conf_path = config_path + "/topologies/" + subdir + "/topology.conf";
-		String environment_identifier_path = config_path + "/topologies/environment_identifier.conf";
-		String topology_identifier_path =  config_path + "/topologies/" + subdir + "/topology_identifier.conf";
-		
-		System.out.println("[OpenSOC] Looking for environment identifier: " + environment_identifier_path);
-		System.out.println("[OpenSOC] Looking for topology identifier: " + topology_identifier_path);
-		System.out.println("[OpenSOC] Looking for topology config: " + topology_conf_path);
-		
+		String topology_conf_path = config_path + "/topologies/" + subdir
+				+ "/topology.conf";
+		String environment_identifier_path = config_path
+				+ "/topologies/environment_identifier.conf";
+		String topology_identifier_path = config_path + "/topologies/" + subdir
+				+ "/topology_identifier.conf";
+
+		System.out.println("[OpenSOC] Looking for environment identifier: "
+				+ environment_identifier_path);
+		System.out.println("[OpenSOC] Looking for topology identifier: "
+				+ topology_identifier_path);
+		System.out.println("[OpenSOC] Looking for topology config: "
+				+ topology_conf_path);
+
 		config = new PropertiesConfiguration(topology_conf_path);
 
 		JSONObject environment_identifier = SettingsLoader
@@ -71,23 +87,72 @@ public class PCAPTopology {
 		conf = new Config();
 		conf.registerSerialization(JSONObject.class, JSONKryoSerializer.class);
 		conf.setDebug(config.getBoolean("debug.mode"));
-		
+
 		if (config.getBoolean("spout.generator.enabled", false)) {
 			String component_name = config.getString("spout.generator.name",
 					"DefaultTopologySpout");
-			success = initializeGeneratorSpout("SampleInput/LancopeExampleOutput",
+			success = initializeGeneratorSpout("SampleInput/PcapExampleOutput",
 					component_name);
 			component = component_name;
 
 			System.out.println("[OpenSOC] Component " + component
 					+ " initialized");
 		}
+
+		if (config.getBoolean("spout.kafka.enabled", true)) {
+
+			String component_name = config.getString("spout.kafka.name",
+					"KafkaSpout");
+			success = initializeKafkaSpout(component_name);
+			component = component_name;
+			
+			System.out.println("[OpenSOC] Component " + component
+					+ " initialized");
+
+		}
+
 	}
 
-	private static boolean initializeGeneratorSpout(String string,
-			String component_name) {
-		// TODO Auto-generated method stub
-		return false;
+	public static boolean initializeGeneratorSpout(String file_path, String name) {
+
+		return true;
+	}
+
+	public static boolean initializeKafkaSpout(String name) {
+		try {
+
+			BrokerHosts zk = new ZkHosts(config.getString("kafka.zk"));
+			String input_topic = config.getString("spout.kafka.topic");
+			String instance_id = config.getString("instance.id");
+			String zk_root = config.getString("spout.kafka.zk.root");
+
+			SpoutConfig kafkaConfig = new SpoutConfig(zk, input_topic, zk_root,
+					instance_id);
+			kafkaConfig.scheme = new SchemeAsMultiScheme(new RawScheme());
+			kafkaConfig.forceFromStart = config.getBoolean(
+					"kafka.spout.forcefromstart", false);
+			kafkaConfig.startOffsetTime = config
+					.getLong("spout.kafka.start.offset.time");
+
+			kafkaConfig.bufferSizeBytes = config
+					.getInt("spout.kafka.buffer.size.bytes");
+			kafkaConfig.fetchSizeBytes = config
+					.getInt("spout.kafka.fetch.size.bytes");
+			kafkaConfig.socketTimeoutMs = config
+					.getInt("spout.kafka.socket.timeout.ms");
+
+			// kafkaConfig.scheme = new RawMultiScheme();
+
+			builder.setSpout(name, new KafkaSpout(kafkaConfig),
+					config.getInt("spout.kafka.parallelism.hint")).setNumTasks(
+					config.getInt("spout.kafka.num.tasks"));
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		return true;
 	}
 
 }

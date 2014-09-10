7b83c087c343bea85f178f491715e0369a02e9d8
Ratna Piddaparti <spiddapa@cisco.com>
2014-09-10 11:19:42 -0700

    fixed issues with Bro parser test

--- OpenSOC-MessageParsers/src/test/java/com/opensoc/parsing/test/BroParserTest.java
+++ OpenSOC-MessageParsers/src/test/java/com/opensoc/parsing/test/BroParserTest.java
@@ -41,7 +41,7 @@ public class BroParserTest extends TestCase {
 	 * @throws java.lang.Exception
 	 */
 	public static void setUpBeforeClass() throws Exception {
-		setBroJsonString("{\"http\":{\"ts\":1402307733473,\"uid\":\"CTo78A11g7CYbbOHvj\",\"id.orig_h\":\"192.249.113.37\",\"id.orig_p\":58808,\"id.resp_h\":\"72.163.4.161\",\"id.resp_p\":80,\"trans_depth\":1,\"method\":\"GET\",\"host\":\"www.cisco.com\",\"uri\":\"/\",\"user_agent\":\"curl/7.22.0 (x86_64-pc-linux-gnu) libcurl/7.22.0 OpenSSL/1.0.1 zlib/1.2.3.4 libidn/1.23 librtmp/2.3\",\"request_body_len\":0,\"response_body_len\":25523,\"status_code\":200,\"status_msg\":\"OK\",\"tags\":[],\"resp_fuids\":[\"FJDyMC15lxUn5ngPfd\"],\"resp_mime_types\":[\"text/html\"]}}");
+		
 	}
 
 	/**
@@ -55,6 +55,7 @@ public class BroParserTest extends TestCase {
 	 * @throws java.lang.Exception
 	 */
 	public void setUp() throws Exception {
+	    setBroJsonString("{\"http\":{\"ts\":1402307733473,\"uid\":\"CTo78A11g7CYbbOHvj\",\"id.orig_h\":\"192.249.113.37\",\"id.orig_p\":58808,\"id.resp_h\":\"72.163.4.161\",\"id.resp_p\":80,\"trans_depth\":1,\"method\":\"GET\",\"host\":\"www.cisco.com\",\"uri\":\"/\",\"user_agent\":\"curl/7.22.0 (x86_64-pc-linux-gnu) libcurl/7.22.0 OpenSSL/1.0.1 zlib/1.2.3.4 libidn/1.23 librtmp/2.3\",\"request_body_len\":0,\"response_body_len\":25523,\"status_code\":200,\"status_msg\":\"OK\",\"tags\":[],\"resp_fuids\":[\"FJDyMC15lxUn5ngPfd\"],\"resp_mime_types\":[\"text/html\"]}}");	    
 		assertNotNull(getBroJsonString());
 		BroParserTest.setBroParser(new BasicBroParser());		
 	}
@@ -62,15 +63,18 @@ public class BroParserTest extends TestCase {
 	/**
 	 * @throws ParseException
 	 * Tests for Parse Method
-	 * Parses Static json Stirng and checks if any spl chars are present in parsed string.
+	 * Parses Static json String and checks if any spl chars are present in parsed string.
 	 */
 	@SuppressWarnings({ "unused", "rawtypes" })
 	public void testParse() throws ParseException {
 
 
 		BasicBroParser broparser = new BasicBroParser();
+		assertNotNull(getBroJsonString());
 		JSONObject cleanJson = broparser.parse(getBroJsonString().getBytes());
+        assertNotNull(cleanJson);		
 		System.out.println(cleanJson);
+
 
 		Pattern p = Pattern.compile("[^\\._a-z0-9 ]", Pattern.CASE_INSENSITIVE);
 
@@ -107,7 +111,7 @@ public class BroParserTest extends TestCase {
      * Return BroPaser JSON String
      */
 	public static String getBroJsonString() {
-		return broJsonString;
+		return BroParserTest.broJsonString;
 	}
 
     /**

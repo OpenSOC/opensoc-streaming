5995cfcecb84ca52a8decf76a92554f27da5b028
Kiran K <kiran@tupl.io>
2014-09-15 09:59:53 -0700

    Adding Bro Filter
    	modified:   OpenSOC-Common/src/main/java/com/opensoc/json/serialization/JSONKafkaSerializer.java
    	modified:   OpenSOC-MessageParsers/src/main/java/com/opensoc/filters/BroMessageFilter.java
    	modified:   OpenSOC-Topologies/src/main/java/com/opensoc/topologies/BroEnrichmentTestTopology.java

--- OpenSOC-MessageParsers/src/main/java/com/opensoc/filters/BroMessageFilter.java
+++ OpenSOC-MessageParsers/src/main/java/com/opensoc/filters/BroMessageFilter.java
@@ -10,18 +10,51 @@ import org.json.simple.JSONObject;
 
 import com.opensoc.parser.interfaces.MessageFilter;
 
-public class BroMessageFilter implements MessageFilter,Serializable {
+public class BroMessageFilter implements MessageFilter, Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private final String _key = "protocol";
-	private final Set<String> _known_protocols;
+	private final Set<String> _include_protocols, _exclude_protocols;
+	private boolean include_all = false;
+	private boolean exclude_none = false;
 
 	public BroMessageFilter(Configuration conf) {
-		_known_protocols = new HashSet<String>();
-		List known_protocols = conf.getList("source.known.protocols");
-		_known_protocols.addAll(known_protocols);
+		_include_protocols = new HashSet<String>();
+		_exclude_protocols = new HashSet<String>();
+		List include_protocols = conf.getList("source.include.protocols", null);
+		List exclude_protocols = conf.getList("source.exclude.protocols", null);
+
+		if (null == include_protocols)
+			include_all = true;
+		else
+			_include_protocols.addAll(include_protocols);
+
+		if (null == exclude_protocols)
+			exclude_none = true;
+		else
+			_exclude_protocols.addAll(exclude_protocols);
+
+		if (_include_protocols.contains("*"))
+			include_all = true;
 	}
 
 	public boolean emitTuple(JSONObject message) {
-		return _known_protocols.contains(message.get(_key));
+		if (include_all)
+			return true;
+
+		if (_include_protocols.contains(message.get(_key)))
+			return true;
+
+		if (exclude_none)
+			return true;
+
+		if (_exclude_protocols.contains(message.get(_key)))
+			return false;
+
+		return false;
+
 	}
 }

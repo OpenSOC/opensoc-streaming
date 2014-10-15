270aee9b0a4701b5e9054ea1c114031a740e80a1
james-sirota <james.sirota@gmail.com>
2014-10-15 03:12:00 -0500

    Added alerts fixes

--- OpenSOC-Alerts/src/main/java/com/opensoc/alerts/adapters/AllAlertAdapter.java
+++ OpenSOC-Alerts/src/main/java/com/opensoc/alerts/adapters/AllAlertAdapter.java
@@ -30,109 +30,87 @@ import com.opensoc.alerts.interfaces.AlertsAdapter;
 
 public class AllAlertAdapter implements AlertsAdapter, Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4963668606839437124L;
+	Cache<String, String> cache;
 
-	HTableInterface whitelist_table;
-	InetAddressValidator ipvalidator = new InetAddressValidator();
-	String _whitelist_table_name;
-	// String _blacklist_table_name;
-	String _quorum;
-	String _port;
-	String _topologyname;
-	Configuration conf = null;
+	public AllAlertAdapter(Map<String, String> config) {
+		try {
 
-	protected Cache<String, String> cache;
+			int _MAX_CACHE_SIZE = Integer.parseInt(config
+					.get("_MAX_CACHE_SIZE"));
 
-	Map<String, String> id_list = new HashMap<String, String>();
+			if (!config.containsKey("_MAX_TIME_RETAIN"))
+				throw new Exception("_MAX_TIME_RETAIN name is missing");
 
-	Set<String> loaded_whitelist = new HashSet<String>();
-	Set<String> loaded_blacklist = new HashSet<String>();
+			int _MAX_TIME_RETAIN = Integer.parseInt(config
+					.get("_MAX_TIME_RETAIN"));
 
-	String _topology_name;
+			cache = CacheBuilder.newBuilder().maximumSize(_MAX_CACHE_SIZE)
+					.expireAfterWrite(_MAX_TIME_RETAIN, TimeUnit.MINUTES)
+					.build();
+		} catch (Exception e) {
+			System.out.println("Could not initialize alerts adapter");
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
 
-	protected static final Logger LOG = LoggerFactory
-			.getLogger(AllAlertAdapter.class);
+	@Override
+	public boolean initialize() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 
-	public AllAlertAdapter(String whitelist_table_name,
-			String blacklist_table_name, String quorum, String port,
-			int _MAX_TIME_RETAIN, int _MAX_CACHE_SIZE) {
+	@Override
+	public boolean refresh() throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-		_whitelist_table_name = whitelist_table_name;
+	@Override
+	public Map<String, JSONObject> alert(JSONObject raw_message) {
 
-		_quorum = quorum;
-		_port = port;
+		Map<String, JSONObject> alerts = new HashMap<String, JSONObject>();
+		JSONObject content = (JSONObject) raw_message.get("message");
 
-		cache = CacheBuilder.newBuilder().maximumSize(_MAX_CACHE_SIZE)
-				.expireAfterWrite(_MAX_TIME_RETAIN, TimeUnit.MINUTES).build();
+		JSONObject enrichment = null;
+
+		if (raw_message.containsKey("enrichment"))
+			enrichment = (JSONObject) raw_message.get("enrichment");
+
+		JSONObject alert = new JSONObject();
+
+		alert.put("title", "Alert from Topology");
+		alert.put("priority", "4");
+		alert.put("type", "error");
+		alert.put("designated_host", "Uknown");
+		alert.put("source", "NA");
+		alert.put("dest", "NA");
+		alert.put("body", "Alert from Topology");
+
+		String source = "unknown";
+		String dest = "unknown";
+
+		if (content.containsKey("ip_src_addr"))
+			source = content.get("ip_src_addr").toString();
+
+		if (content.containsKey("ip_dst_addr"))
+			dest = content.get("ip_dst_addr").toString();
+
+		String alert_id = generateAlertId(source, dest, 0);
+
+		alert.put("reference_id", alert_id);
+		alerts.put(alert_id, alert);
+
+		alert.put("enrichment", enrichment);
+
+		return alerts;
 
 	}
 
-	public boolean initialize() {
-
-		conf = HBaseConfiguration.create();
-		conf.set("hbase.zookeeper.quorum", _quorum);
-		conf.set("hbase.zookeeper.property.clientPort", _port);
-
-		LOG.trace("[OpenSOC] Connecting to hbase with conf:" + conf);
-		LOG.trace("[OpenSOC] Whitelist table name: " + _whitelist_table_name);
-		LOG.trace("[OpenSOC] ZK Client/port: "
-				+ conf.get("hbase.zookeeper.quorum") + " -> "
-				+ conf.get("hbase.zookeeper.property.clientPort"));
-
-		try {
-
-			// HConnection connection =
-			// HConnectionManager.createConnection(conf);
-
-			LOG.trace("[OpenSOC] CONNECTED TO HBASE");
-
-			HBaseAdmin hba = new HBaseAdmin(conf);
-
-			if (!hba.tableExists(_whitelist_table_name))
-				throw new Exception("Whitelist table doesn't exist");
-
-			whitelist_table = new HTable(conf, _whitelist_table_name);
-
-			LOG.trace("[OpenSOC] CONNECTED TO TABLE: " + _whitelist_table_name);
-
-			Scan scan = new Scan();
-
-			ResultScanner rs = whitelist_table.getScanner(scan);
-			try {
-				for (Result r = rs.next(); r != null; r = rs.next()) {
-					loaded_whitelist.add(Bytes.toString(r.getRow()));
-				}
-			} catch (Exception e) {
-				LOG.trace("[OpenSOC] COULD NOT READ FROM HBASE");
-				e.printStackTrace();
-			} finally {
-				rs.close();
-				hba.close();
-			}
-			whitelist_table.close();
-
-			LOG.trace("[OpenSOC] Number of entires in white list: "
-					+ loaded_whitelist.size());
-
-			if (loaded_whitelist.size() == 0)
-				throw new Exception(
-						"Hbase connection is OK, but the table is empty: "
-								+ whitelist_table);
-
-			rs.close();
-			hba.close();
-
-			return true;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+	@Override
+	public boolean containsAlertId(String alert) {
+		// TODO Auto-generated method stub
 		return false;
-
 	}
 
 	protected String generateAlertId(String source_ip, String dst_ip,
@@ -153,87 +131,7 @@ public class AllAlertAdapter implements AlertsAdapter, Serializable {
 
 	}
 
-	public boolean getByKey(String metadata, HTableInterface table) {
-
-		LOG.trace("[OpenSOC] Pinging HBase For:" + metadata);
-
-		Get get = new Get(metadata.getBytes());
-		Result rs;
-
-		try {
-			rs = table.get(get);
-
-			if (rs.size() > 0)
-				return true;
-			else
-				return false;
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-		}
-
-		return false;
-
-	}
-
-	public boolean refresh() throws Exception {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 	private String makeKey(String ip1, String ip2, int alert_type) {
 		return (ip1 + "-" + ip2 + "-" + alert_type);
 	}
-
-	@SuppressWarnings("unchecked")
-	public Map<String, JSONObject> alert(JSONObject raw_message) {
-
-		Map<String, JSONObject> alerts = new HashMap<String, JSONObject>();
-		JSONObject alert = new JSONObject();
-
-		JSONObject content = (JSONObject) raw_message.get("message");
-		JSONObject enrichment = null;
-
-		if (raw_message.containsKey("enrichment"))
-			enrichment = (JSONObject) raw_message.get("enrichment");
-
-		String source_ip = content.get("ip_src_addr").toString();
-		String dst_ip = content.get("ip_dst_addr").toString();
-
-		String source = null;
-
-		if (loaded_whitelist.contains(source_ip))
-			source = source_ip;
-		else if (loaded_whitelist.contains(dst_ip))
-			source = dst_ip;
-		else
-			source = "unknown";
-
-		alert.put("title", "Appliance alert for: " + source_ip + "->" + dst_ip);
-		alert.put("priority", "1");
-		alert.put("type", "error");
-		alert.put("designated_host", source);
-		alert.put("source", source_ip);
-		alert.put("dest", dst_ip);
-		alert.put("body", "Appliance alert for: " + source_ip + "->" + dst_ip);
-
-		if (enrichment != null)
-			alert.put("enrichment", enrichment);
-
-		String alert_id = generateAlertId(source_ip, dst_ip, 0);
-
-		alert.put("reference_id", alert_id);
-		alerts.put(alert_id, alert);
-
-		LOG.trace("[OpenSOC] Returning alert: " + alerts);
-
-		return alerts;
-	}
-
-	public boolean containsAlertId(String alert) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
 }
--- OpenSOC-Alerts/src/main/java/com/opensoc/alerts/adapters/CIFAlertsAdapter.java
+++ OpenSOC-Alerts/src/main/java/com/opensoc/alerts/adapters/CIFAlertsAdapter.java
@@ -4,40 +4,91 @@ import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 import org.json.simple.JSONObject;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.opensoc.alerts.interfaces.AlertsAdapter;
 
 public class CIFAlertsAdapter implements AlertsAdapter, Serializable {
 	
-	public CIFAlertsAdapter(Map<String, String> configs)
+	String enrichment_tag;
+	Cache<String, String> cache;
+	
+	public CIFAlertsAdapter(Map<String, String> config)
 	{
+		try
+		{
+		enrichment_tag = config.get("enrichment_tag");
 		
+		int _MAX_CACHE_SIZE = Integer.parseInt(config
+				.get("_MAX_CACHE_SIZE"));
+		
+		if(!config.containsKey("_MAX_TIME_RETAIN"))
+			throw new Exception("_MAX_TIME_RETAIN name is missing");
+		
+		int _MAX_TIME_RETAIN = Integer.parseInt(config
+				.get("_MAX_TIME_RETAIN"));
+
+		cache = CacheBuilder.newBuilder().maximumSize(_MAX_CACHE_SIZE)
+				.expireAfterWrite(_MAX_TIME_RETAIN, TimeUnit.MINUTES)
+				.build();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Could not initialize alerts adapter");
+			e.printStackTrace();
+			System.exit(0);
+		}
 	}
 
 	@Override
 	public boolean initialize() {
-		return false;
+		return true;
 	}
 
 	@Override
 	public boolean refresh() throws Exception {
-		return false;
+		return true;
 	}
 
 	@Override
 	public Map<String, JSONObject> alert(JSONObject raw_message) {
+		
+		System.out.println("LOOKING FOR ENRICHMENT TAG: " + enrichment_tag);
 
 		Map<String, JSONObject> alerts = new HashMap<String, JSONObject>();
+		JSONObject content = (JSONObject) raw_message.get("message");
+		
+
 
 		JSONObject enrichment = null;
 
 		if (raw_message.containsKey("enrichment"))
 			enrichment = (JSONObject) raw_message.get("enrichment");
 
-		if (enrichment.containsKey("cif")) {
-
+		if (enrichment.containsKey(enrichment_tag)) {
+			
+			System.out.println("FOUND TAG: " + enrichment_tag);
+			
+			JSONObject cif = (JSONObject) enrichment.get(enrichment_tag);
+			
+			int cnt=0;
+			
+			for(Object key : cif.keySet())
+			{
+				JSONObject tmp = (JSONObject) cif.get(key);
+				cnt = cnt + tmp.size();
+			}
+			
+			if(cnt == 0)
+			{
+				System.out.println("TAG HAS NO ELEMENTS");
+				return null;
+			}
+			
 			JSONObject alert = new JSONObject();
 
 			alert.put("title", "Flagged by CIF");
@@ -47,8 +98,17 @@ public class CIFAlertsAdapter implements AlertsAdapter, Serializable {
 			alert.put("source", "NA");
 			alert.put("dest", "NA");
 			alert.put("body", "Flagged by CIF");
+			
+			String source = "unknown";
+			String dest = "unknown";
+			
+			if(content.containsKey("ip_src_addr"))
+				source = content.get("ip_src_addr").toString();
+			
+			if(content.containsKey("ip_dst_addr"))
+				dest = content.get("ip_dst_addr").toString();
 
-			String alert_id = UUID.randomUUID().toString();
+			String alert_id = generateAlertId(source, dest, 0);
 
 			alert.put("reference_id", alert_id);
 			alerts.put(alert_id, alert);
@@ -57,7 +117,10 @@ public class CIFAlertsAdapter implements AlertsAdapter, Serializable {
 
 			return alerts;
 		} else
+		{
+			System.out.println("DID NOT FIND TAG: " + enrichment_tag);
 			return null;
+		}
 
 	}
 
@@ -66,5 +129,26 @@ public class CIFAlertsAdapter implements AlertsAdapter, Serializable {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+	protected String generateAlertId(String source_ip, String dst_ip,
+			int alert_type) {
 
+		String key = makeKey(source_ip, dst_ip, alert_type);
+
+		if (cache.getIfPresent(key) != null)
+			return cache.getIfPresent(key);
+
+		String new_UUID = System.currentTimeMillis() + "-" + UUID.randomUUID();
+
+		cache.put(key, new_UUID);
+		key = makeKey(dst_ip, source_ip, alert_type);
+		cache.put(key, new_UUID);
+
+		return new_UUID;
+
+	}
+	
+	private String makeKey(String ip1, String ip2, int alert_type) {
+		return (ip1 + "-" + ip2 + "-" + alert_type);
+	}
 }

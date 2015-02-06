3dbe418d044584f2197b93a5b900f58ec45b5e32
Dilip Movva <dilip.movva@gmail.com>
2015-02-06 14:13:25 -0500

    set mode to local to make tests pass

--- OpenSOC-EnrichmentAdapters/src/test/java/com/opensoc/enrichment/adapters/cif/CIFHbaseAdapterTest.java
+++ OpenSOC-EnrichmentAdapters/src/test/java/com/opensoc/enrichment/adapters/cif/CIFHbaseAdapterTest.java
@@ -76,6 +76,10 @@ public class CIFHbaseAdapterTest extends AbstractTestContext {
         Properties prop = super.getTestProperties();
         assertNotNull(prop);
         
+        if(skipTests(this.getMode())){
+            return;//skip tests
+        }
+        
         String[] zk = prop.get("kafka.zk.list").toString().split(",");
         
         for(String z : zk)

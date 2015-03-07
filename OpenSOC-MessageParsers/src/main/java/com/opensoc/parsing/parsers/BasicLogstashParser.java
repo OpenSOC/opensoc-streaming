cdbf3c88c5c52120039b146e4a9cdd2b7356497d
Tom Piscitell <tpiscite@cisco.com>
2015-03-07 18:15:15 -0500

    convert timestamps and remove tags in logstash parser

--- OpenSOC-MessageParsers/src/main/java/com/opensoc/parsing/parsers/BasicLogstashParser.java
+++ OpenSOC-MessageParsers/src/main/java/com/opensoc/parsing/parsers/BasicLogstashParser.java
@@ -1,6 +1,7 @@
 package com.opensoc.parsing.parsers;
 
 import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -23,32 +24,40 @@ public class BasicLogstashParser extends AbstractParser {
 			JSONObject rawJson = (JSONObject) jsonParser.parse(rawString);
 			
 			// remove logstash meta fields
-			rawJson.remove("@vesrion");
+			rawJson.remove("@version");
 			rawJson.remove("type");
 			rawJson.remove("host");
+			rawJson.remove("tags");
 			
 			// rename other keys
-			rawJson = mutate(rawJson, "@timestamp", "timestamp");
 			rawJson = mutate(rawJson, "message", "original_string");
 			rawJson = mutate(rawJson, "src_ip", "ip_src_addr");
 			rawJson = mutate(rawJson, "dst_ip", "ip_dst_addr");
 			rawJson = mutate(rawJson, "src_port", "ip_src_port");
 			rawJson = mutate(rawJson, "dst_port", "ip_dst_port");
 			rawJson = mutate(rawJson, "src_ip", "ip_src_addr");
+			
+			// convert timestamp to milli since epoch
+			rawJson.put("timestamp", LogstashToEpoch((String) rawJson.remove("@timestamp")));
 
 			return rawJson;
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
-		}
-		
+		}	
 	}
 	
 	private JSONObject mutate(JSONObject json, String oldKey, String newKey) {
 		if (json.containsKey(oldKey)) {
 			json.put(newKey, json.remove(oldKey));
-		}
+		}	
 		return json;
+	}
+	
+	private long LogstashToEpoch(String timestamp) throws java.text.ParseException {
+		SimpleDateFormat logstashDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+		return logstashDateFormat.parse(timestamp).getTime();
+		
 	}
 
 }
